30f2906730bef90091433570609d72e72f484672
Hassan Amjad <hassanamjad77@gmail.com>
2018-04-01 15:06:11 +0500

    Babel Configuration and Running

---
+++ public/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+console.log('App.js is running!');
+
+// JSX - JavaScript XML
+var template = React.createElement(
+  'h1',
+  null,
+  'Does this change?'
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
