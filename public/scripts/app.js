c297a72b5114351101e36b0def10060bc69db53b
Hassan Amjad <hassanamjad77@gmail.com>
2018-04-02 02:42:56 +0500

    Conditional Rendering Techniques

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,12 +3,60 @@
 console.log('App.js is running!');
 
 // JSX - JavaScript XML
+
+var app = {
+    title: 'Indecision App',
+    subtitile: 'My first React.JS App',
+    options: ['one', 'two']
+};
+
+var user = {
+    name: 'Hassan',
+    age: 19,
+    location: 'Lahore, PK'
+};
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location
+        );
+    }
+}
 var template = React.createElement(
-  'h1',
-  null,
-  'Does this change?'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name ? user.name : 'Anonymous'
+    ),
+    user.age && user.age >= 18 && React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    getLocation(user.location)
+);
+var template2 = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    app.subtitile && React.createElement(
+        'p',
+        null,
+        app.subtitile
+    ),
+    app.options.length > 0 ? 'Here are your options' : 'You have no options'
 );
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,9 +1,40 @@
 console.log('App.js is running!');
 
 // JSX - JavaScript XML
-var template = <h1>Does this change?</h1>;
+
+var app = {
+    title:'Indecision App',
+    subtitile: 'My first React.JS App',
+    options: ['one', 'two']
+};
+
+var user = {
+    name: 'Hassan',
+    age: 19,
+    location: 'Lahore, PK'
+}
+function getLocation(location){
+    if(location){
+        return <p>Location: {location}</p>;
+    }
+}
+var template = (
+    <div>
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        {(user.age && user.age >=18) && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
+    </div>
+);
+var template2 = (
+    <div>
+        <h1>{app.title}</h1>
+        {app.subtitile && <p>{app.subtitile}</p>}
+        {app.options.length>0 ? 'Here are your options': 'You have no options'}
+
+    </div>
+);
 
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot);
