b3e49d332b6ffd931be1499aae5316a21590f7ac
Hassan Amjad <hassanamjad77@gmail.com>
2018-04-02 13:28:38 +0500

    ES6 Arrow Functions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,62 +1,12 @@
 'use strict';
 
-console.log('App.js is running!');
-
-// JSX - JavaScript XML
-
-var app = {
-    title: 'Indecision App',
-    subtitile: 'My first React.JS App',
-    options: ['one', 'two']
+var getFirstName = function getFirstName(name) {
+    return name.split(' ')[0];
 };
 
-var user = {
-    name: 'Hassan',
-    age: 19,
-    location: 'Lahore, PK'
+var getFirstName2 = function getFirstName2(name) {
+    return name.split(' ')[0];
 };
-function getLocation(location) {
-    if (location) {
-        return React.createElement(
-            'p',
-            null,
-            'Location: ',
-            location
-        );
-    }
-}
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        user.name ? user.name : 'Anonymous'
-    ),
-    user.age && user.age >= 18 && React.createElement(
-        'p',
-        null,
-        'Age: ',
-        user.age
-    ),
-    getLocation(user.location)
-);
-var template2 = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        app.title
-    ),
-    app.subtitile && React.createElement(
-        'p',
-        null,
-        app.subtitile
-    ),
-    app.options.length > 0 ? 'Here are your options' : 'You have no options'
-);
 
-var appRoot = document.getElementById('app');
-
-ReactDOM.render(template2, appRoot);
+console.log(getFirstName('Hassan Amjad'));
+console.log(getFirstName2('Hassan Amjad'));
---
+++ src/playground/es6-arrow-function.js
@@ -0,0 +1,9 @@
+const getFirstName = (name) => {
+    return name.split(' ')[0];
+}
+
+const getFirstName2 = (name) => name.split(' ')[0];
+
+
+console.log(getFirstName('Hassan Amjad'));
+console.log(getFirstName2('Hassan Amjad'));
