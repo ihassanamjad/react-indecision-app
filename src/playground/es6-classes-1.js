13cbcd6a7ffed829a8292e903daf16cee52fbbc0
Hassan Amjad <hassanamjad77@gmail.com>
2018-04-03 14:23:14 +0500

    ES6 Subclasses and Function Overriding

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -1,17 +1,57 @@
-
-
 class Person {
     constructor(name = 'Anonymous', age = 0){
         this.name = name;
         this.age = age;
+    }
+    getGreeting(){
+        return `Hi. I am ${this.name}. `;
     }
     getDescription(){
         return `${this.name} is ${this.age} year(s) old!`;
     }
 }
 
-const me = new Person('Hassan Amjad', 23);
-console.log(me.getDescription());
+class Student extends Person {
+    constructor(name, age, major){
+        super(name, age);
+        this.major = major;
+    }
 
-const other = new Person();
-console.log(other.getDescription());
+    hasMajor(){
+        return !!this.major;
+    }
+
+    getDescription(){
+        let description = super.getDescription();
+        if(this.hasMajor()){
+            description += `Their major is ${this.major}`;
+        }
+        
+        return description;
+    }
+}
+
+class Traveler extends Person{
+    constructor(name, age, homeLocation){
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+
+    hasHomeLocation(){
+        return !!this.homeLocation;
+    }
+
+    getGreeting(){
+        let greeting = super.getGreeting();
+        if(this.hasHomeLocation()){
+            greeting += `I'm visiting from ${this.homeLocation}`;
+        }
+
+        return greeting;
+    }
+}
+const me = new Traveler('Hassan Amjad', 23, 'Lahore, Pakistan');
+console.log(me.getGreeting());
+
+const other = new Traveler();
+console.log(other.getGreeting());
